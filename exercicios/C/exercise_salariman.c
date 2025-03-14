0650c716677222f8b052b2e0fc24205bada41a9e
InsanityAddiction <fgabriel2011@gmail.com>
2025-03-14 19:07:37 -0300

    exercises in C

---
+++ exercicios/C/exercise_salariman.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+char ouro = 'o';
+char pedra = 'p';
+char troca;
+
+printf("Chiba City, 21xx\n\n");
+
+ printf("Um Salaryman ricasso estava adormecido em uma mesa do Chatsuba.\n\n");
+ 
+ printf("Em outra mesa estao Case e Ratz.\n\n");
+  printf("-Case, olhe para aquele dorminhoco.' aponta o dedo para o Salaryman\n\n -Esta totalmente louco em vir para esse bar com aquela maleta brilhante.\n\n");
+   printf("-Eu o conheco, e um ricasso que aplicava vários golpes em inocentes no Sprawl. disse Case\n\n");
+    printf("-Vou fazer com que pague.\n\n Case da um ultimo gole da cerveja antes de se levantar.\n\n");
+     printf("-Te encontro depois Ratz.\n\n -Ate logo. disse Ratz\n\n");
+      printf("Case se espreita ate a mesa do Salaryman ricasso.\n\n\n");
+       printf("Mantendo a calma ele pega a maleta e analisa o conteudo\n\n");
+
+  printf("Conteudo da maleta: %c\n\n",ouro);
+   printf("-Isso e exatamento o que preciso para quitar minha divida.\n\n -Agora vou subststituir por alguma coisa que faça peso na maleta para que ele nao desconfie ao acordar.\n\n");
+ 
+  printf("Case pega algumas pedras que estavam no chao\n\n");
+   printf("-Perfeito. Vamos trabalhar nisso.\n\n");
+    printf("Conteudo do bolso de case: %c\n\n", pedra);
+
+  printf("'O que Case fara a seguir?\n\n");
+
+  scanf("%f");
+   printf("Case cautelosamente troca as joias de ouro por pedras de seu bolso.\n\n");
+  
+  troca = ouro;
+  ouro = pedra;
+  pedra = troca;
+
+  printf("Conteudo da maleta: %c\n\n", ouro);
+   printf("Conteudo do bolso de case %c\n\n\n", pedra);
+    printf("-Boa sorte para recuperar isso, filho da puta.\n\n\n");
+     printf("Apos sucesso do furto Case vai embora do Chatsuba\n\n");
+      printf(" FIM\n");
+
+}
